8b66eaef1a971e921505409d454a9202d2077ef4
wlsgml0229 <wlsgml0229@naver.com>
2024-06-22 00:01:04 +0900

    💄 헤더 디자인 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,27 @@
 import React from "react"
 import { Image } from "next/image"
+import { FaPen } from "react-icons/fa"
+import { FaClock } from "react-icons/fa6"
+import { SlSpeech } from "react-icons/sl"
 
 const Header = () => {
-  const 
   return (
-    <header className="bg-gray-800 flex justify-end">
-      <span className="flex p-4 cursor-pointer gap-3 text-white flex sm:hidden">
-        🍔
-      </span>
-
-      <ul className="flex p-4 cursor-pointer gap-3 text-white hidden sm:flex">
-        <li>기록</li>
-        <li>뽀모도로</li>
-        <li>공유</li>
+    <nav className="fixed bottom-0 left-0 z-50 w-full h-fit bg-white border-t border-gray-100 dark:bg-gray-700 dark:border-gray-600">
+      <ul className="flex w-full p-4 cursor-pointer gap-3 text-gray sm:flex">
+        <li className="flex-1 flex-col flex items-center gap-2">
+          <FaPen />
+          <span>일정</span>
+        </li>
+        <li className="flex-1 flex-col flex items-center gap-2">
+          <FaClock />
+          <span>뽀모도로</span>
+        </li>
+        <li className="flex-1 flex-col flex items-center gap-2">
+          <SlSpeech />
+          <span>공유</span>
+        </li>
       </ul>
-    </header>
+    </nav>
   )
 }
 
